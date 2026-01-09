4a777a7b7bba26ba6907be227ed1ba575fee8e67
Medhachalla <sadhana.challa@gmail.com>
2026-01-09 11:07:57 +0530

    Revert "dockerized everything"

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,11 @@
 import psycopg2
-import os
-
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("POSTGRES_HOST", "db"),
-        database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        port=os.getenv("POSTGRES_PORT", "5432")
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
     )
-
